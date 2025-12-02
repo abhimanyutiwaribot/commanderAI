1becb6b7eab62cb60d93a37952073768b1f7637f
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2025-12-02 22:01:00 +0530

    changes in hero ui

--- frontend/src/components/hero-ui.jsx
+++ frontend/src/components/hero-ui.jsx
@@ -1,10 +1,10 @@
 export default function Hero() {
     return (
         <div className="text-center">
-            <div className="text-opacity-90 text-gray-800 text-[25px]">
+            <div className="text-opacity-90 text-gray-500 text-[25px]">
                 Chat with agent
             </div>
-            <div className="text-opacity-55 text-blue-700 font-medium text-[45px]">
+            <div className="text-opacity-55 text-emerald-700 font-medium text-[45px]">
                 CommanderAI
             </div>
         </div>
